b8c5dd539394432ac0c6059ecf27eb5bb3bfbaa5
nothingelsematters <daretoodefy@gmail.com>
2019-10-20 19:18:54 +0300

    :star2: guake magic

--- nixpkgs/services/guake/default.nix
+++ nixpkgs/services/guake/default.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ...}:
 
 {
-  home.packages = [ pkgs.guake ];
+  home.packages = [
+    (pkgs.guake.overrideAttrs (base: {
+      propagatedBuildInputs = base.propagatedBuildInputs ++ [ pkgs.python37Packages.packaging ];
+    }))
+  ];
   systemd.user.services.guake = {
     Install = {
       WantedBy = [ "graphical-session.target" ];
