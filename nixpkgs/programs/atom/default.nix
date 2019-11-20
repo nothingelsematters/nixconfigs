973affcd1c758b62480837e4e9ddadd09ded693b
nothingelsematters <daretoodefy@gmail.com>
2019-11-20 19:48:48 +0300

    :recycle: atom program refactoring

--- nixpkgs/programs/atom/default.nix
+++ nixpkgs/programs/atom/default.nix
@@ -1,193 +1,55 @@
+{ pkgs, ... }:
+
+let
+  theme = import ../../themes { inherit pkgs; };
+in
 {
-  home.file.".atom/config.cson".text =
-    ''
-    "*":
-      "atom-ide-ui":
-        hyperclick:
-          linuxTriggerKeys: "altKey"
-        use:
-          "atom-ide-busy-signal": "never"
-          "atom-ide-code-actions": "never"
-          "atom-ide-code-format": "never"
-          "atom-ide-console": "never"
-          "atom-ide-debugger": "never"
-          "atom-ide-diagnostics": "never"
-          "atom-ide-diagnostics-ui": "never"
-          "atom-ide-find-references": "never"
-          "atom-ide-global": "never"
-          "atom-ide-outline-view": "always"
-          "atom-ide-refactor": "never"
-          "atom-ide-signature-help": "never"
-          "atom-ide-terminal": "never"
-          hyperclick: "always"
-      "autocomplete-clang":
-        clangCommand: "clang++"
-        "preCompiledHeaders c++": [
-          "cassert"
-          "cerrno"
-          "cmath"
-          "cstddef"
-          "cstdio"
-          "cstdlib"
-          "cstring"
-          "ctime"
-          "deque"
-          "list"
-          "map"
-          "queue"
-          "set"
-          "stack"
-          "vector"
-          "fstream"
-          "iomanip"
-          "ios"
-          "iostream"
-          "istream"
-          "ostream"
-          "sstream"
-          "algorithm"
-          "bitset"
-          "complex"
-          "exception"
-          "functional"
-          "iterator"
-          "limits"
-          "memory"
-          "new"
-          "numeric"
-          "stdexcept"
-          "string"
-          "typeinfo"
-          "utility"
-        ]
-        "std c++": "c++17"
-      core:
-        autoHideMenuBar: true
-        disabledPackages: [
-          "language-gfm"
-          "about"
-          "autocomplete-atom-api"
-          "autocomplete-css"
-          "autocomplete-html"
-          "autoflow"
-          "autosave"
-          "background-tips"
-          "bookmarks"
-          "command-palette"
-          "dalek"
-          "deprecation-cop"
-          "exception-reporting"
-          "image-view"
-          "incompatible-packages"
-          "keybinding-resolver"
-          "language-clojure"
-          "language-coffee-script"
-          "language-csharp"
-          "language-go"
-          "language-html"
-          "language-hyperlink"
-          "language-javascript"
-          "language-less"
-          "language-mustache"
-          "language-objective-c"
-          "language-perl"
-          "language-php"
-          "language-property-list"
-          "language-ruby"
-          "language-ruby-on-rails"
-          "language-rust-bundled"
-          "language-sass"
-          "language-sql"
-          "language-toml"
-          "language-typescript"
-          "language-xml"
-          "language-yaml"
-          "link"
-          "markdown-preview"
-          "metrics"
-          "package-generator"
-          "open-on-github"
-          "styleguide"
-          "welcome"
-          "wrap-guide"
-          "spell-check"
-          "timecop"
-          "dev-live-reload"
-          "autocomplete-haskell"
-          "haskell-ghc-mod"
-          "atom-latex"
-          "docblockr"
-          "ide-java"
-        ]
-        reopenProjectMenuCount: 8
-        restorePreviousWindowsOnStart: "no"
-        telemetryConsent: "no"
-        themes: [
-          "material-ui"
-          "material-monokai-syntax"
-        ]
-      editor:
-        fontFamily: "Hasklig Medium"
-        fontSize: 10
-        showIndentGuide: true
-        tabLength: 4
-      "markdown-preview-enhanced":
-        previewTheme: "github-dark.css"
-      minimap:
-        plugins:
-          cursorline: true
-          cursorlineDecorationsZIndex: 0
-          "git-diff": true
-          "git-diffDecorationsZIndex": 0
-          selection: true
-          selectionDecorationsZIndex: 0
-          "split-diff": true
-          "split-diffDecorationsZIndex": 0
-      welcome:
-        showOnStartup: false
-    ".nix":
-      editor:
-        tabLength: 2
-    ".g4":
-      editor:
-        showIndentGuide: false
-    ".hs":
-      editor:
-        showIndentGuide: false
-    '';
+  home.file.".atom/config.cson".text = builtins.readFile ./config.cson;
+  home.file.".atom/snippets.cson".text = builtins.readFile ./snippets.cson;
+  
+  home.file.".atom/packages/atom-material-ui/styles/user-settings.less".text =
+  ''
+  @accent-color: ${theme.colors.background.primary};
+  @accent-text-color: ${theme.colors.text.selection};
+  @base-color: ${theme.colors.background.primary};
+  '';
 
-  home.file.".atom/snippets.cson".text =
-    ''
-    ".source.cpp":
-      "for size_t":
-        "prefix": "fors"
-        "body": """
-          for (size_t $\{1:i} = $\{2:0}; $\{1:i} < $\{3:size}; ++$\{1:i}) {
-              $\{4:/*body*/}
-          }
-        """
-      "for auto":
-        "prefix": "fora"
-        "body": """
-          for (auto$\{2:} $\{1:name} : $\{3:container}) {
-              $\{4:/*body*/}
-          }
-        """
-      "include iostream":
-        "prefix": "ios"
-        "body": "#include <iostream>"
-      "include fstream":
-        "prefix": "fst"
-        "body": "#include <fstream>"
-      "simple main":
-        "prefix": "mains"
-        "body": """
-          int main() {
-              $\{1:/*code*/}
+  home.file.".atom/packages/material-monokai-syntax/styles/syntax-variables.less".text =
+  ''
+  @import "colors";
+  // This defines all syntax variables that syntax themes must implement when they
+  // include a syntax-variables.less file.
 
-              return 0;
-          }
-        """
+  @syntax-cursor-line: hsla(220, 100%,  80%, .04); // needs to be semi-transparent to show serach results
+  @syntax-bracket-matcher-background-color: lighten(@syntax-background-color, 8%);
 
-    '';
+  // General colors
+  @syntax-text-color: #CDD3DE;
+  @syntax-cursor-color: #c0c5ce;
+  @syntax-selection-color: rgba(128, 203, 196, 0.13);
+  @syntax-background-color: ${theme.colors.background.secondary};
+  @syntax-comments-color: #506E79;
+
+  // Guide colors
+  @syntax-wrap-guide-color: #65737e;
+  @syntax-indent-guide-color: #65737e;
+  @syntax-invisible-character-color: #65737e;
+
+  // For find and replace markers
+  @syntax-result-marker-color: #65737e;
+  @syntax-result-marker-color-selected: #CDD3DE;
+
+  // Gutter colors
+  @syntax-gutter-text-color: @syntax-comments-color;
+  @syntax-gutter-text-color-selected: #CDD3DE;
+  @syntax-gutter-background-color: ${theme.colors.background.primary};
+  @syntax-gutter-background-color-selected: rgba(0, 0, 0, 0.19);
+
+  // For git diff info. i.e. in the gutter
+  // These are static and were not extracted from your textmate theme
+  @syntax-color-renamed: #64B5F6;
+  @syntax-color-added: @green;
+  @syntax-color-modified: @yellow;
+  @syntax-color-removed: @red;
+  '';
 }
