35c9cc53e0f0e76506587c98243b133e3ff812a6
nothingelsematters <daretoodefy@gmail.com>
2019-12-27 10:49:24 +0300

    :art: moving polybar configs

--- nixpkgs/services/polybar/default.nix
+++ nixpkgs/services/polybar/default.nix
@@ -3,7 +3,7 @@
 let
   theme = import ../../themes { inherit pkgs; };
   mkINI = import ../../themes/lib/mkINI.nix;
-  cfg = builtins.readFile ./config;
+  cfg = builtins.readFile ./config.ini;
 in
 {
   services.polybar = {
@@ -17,33 +17,6 @@ in
       background-primary-opaque = ${"#66" + builtins.substring 1 6 theme.colors.background.primary}
       ${cfg}
     '';
-    script = "PATH=$PATH:${pkgs.i3}/bin polybar bottom &";
-  };
-
-  xdg.configFile.mpris = {
-    target = "polybar/mpris.sh";
-    executable = true;
-    text =
-      ''
-      #!${pkgs.bash}/bin/bash
-      # Specifying the icon(s) in the script
-      # This allows us to change its appearance conditionally
-      icon=""
-
-        player_status=$(${pkgs.playerctl}/bin/playerctl status 2> /dev/null)
-
-      if [[ $? -eq 0 ]]; then
-          metadata="$(${pkgs.playerctl}/bin/playerctl metadata artist 2> /dev/null) :: $(${pkgs.playerctl}/bin/playerctl metadata title 2> /dev/null)"
-      fi
-
-      # Foreground color formatting tags are optional
-      if [[ $player_status = "Playing" ]]; then
-          echo "%{F#eee}$icon $metadata"       # Orange when playing
-      elif [[ $player_status = "Paused" ]]; then
-          echo "%{F#d1d1d1}$icon $metadata"       # Greyed out info when paused
-      else
-          echo "%{F#65737E}$icon"                 # Greyed out icon when stopped
-      fi
-      '';
+    script = "PATH=$PATH:${pkgs.i3}/bin polybar top &";
   };
 }
