01f880e46b2ef32fd66f4e5030159ac6ad3c7ac3
nothingelsematters <daretoodefy@gmail.com>
2019-12-29 03:04:19 +0300

    :fire: final changes to check-battery service

--- nixpkgs/services/check-battery/default.nix
+++ nixpkgs/services/check-battery/default.nix
@@ -4,17 +4,16 @@ let
   startScript = pkgs.writeShellScriptBin "check-battery.sh" ''
     ACPI=${pkgs.acpi}/bin/acpi
     GREP=${pkgs.ripgrep}/bin/rg
-    BATTINFO=`$ACPI -b`
     NOTIFY=${pkgs.notify-desktop}/bin/notify-desktop;
     SLEEP=${pkgs.coreutils}/bin/sleep
     CUT=${pkgs.coreutils}/bin/cut
 
     while :
     do
-        DISCHARGING=`echo $BATTINFO | $GREP Discharging`
-        CHARGING=`echo $BATTINFO | $GREP Charging`
-        PROCENT=`echo $BATTINFO | $CUT -f 4 -d " " | $CUT -f 1 -d "%"`
-        INFO=`echo $BATTINFO | $CUT -f 3- -d " "`
+        DISCHARGING=`$ACPI -b | $GREP Discharging`
+        CHARGING=`$ACPI -b | $GREP Charging`
+        PROCENT=`$ACPI -b | $CUT -f 4 -d " " | $CUT -f 1 -d "%"`
+        INFO=`$ACPI -b | $CUT -f 3- -d " "`
 
         if [[ $DISCHARGING && $PROCENT -lt 10 ]] ; then
             DISPLAY=:0 $NOTIFY -u critical "critically low battery" "$INFO"
