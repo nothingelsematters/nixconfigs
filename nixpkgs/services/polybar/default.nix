1717d5cb5bef6ebc3eaeab5c5e5ac15640e3f704
nothingelsematters <daretoodefy@gmail.com>
2020-01-12 23:46:31 +0300

    :art: polybar: another icons tweaks go again

--- nixpkgs/services/polybar/default.nix
+++ nixpkgs/services/polybar/default.nix
@@ -14,13 +14,14 @@ in
     };
     extraConfig = ''
       ${mkINI theme.colors}
-      background-primary-opaque = ${"#AA" + builtins.substring 1 6 theme.colors.background.primary}
+      background-primary-opaque = ${"#EE" + builtins.substring 1 6 theme.colors.background.primary}
       ${cfg}
     '';
     script = "PATH=$PATH:${pkgs.i3}/bin:${pkgs.rofi}/bin:${pkgs.networkmanager_dmenu}/bin:${pkgs.bash}/bin"
       + " LABEL_CHARGING=\" %{F${theme.colors.text.primary}}%percentage%%%{F-}\""
       + " LABEL_MUTED=\" %{F${theme.colors.text.primary}}mute%{F-}\""
       + " LABEL_DISCONNECTED=\"%{A1:networkmanager_dmenu:} %{F${theme.colors.text.primary}}[not connected]%{F-}%{A}\""
+      + " FORMAT_CONNECTED=\"%{A1:networkmanager_dmenu:}%{F${theme.colors.text.secondary}}  <ramp-signal> <label-connected>%{F-}%{A}\""
       + " polybar top &";
   };
 }
