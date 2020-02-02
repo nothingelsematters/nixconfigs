36a91a792331745712285abd8d78626aea217340
nothingelsematters <daretoodefy@gmail.com>
2020-02-02 03:44:51 +0300

    :fire: firefox: theme tricks

--- nixpkgs/programs/firefox/default.nix
+++ nixpkgs/programs/firefox/default.nix
@@ -3,6 +3,7 @@
 let
   theme = import ../../theme { inherit pkgs; };
   mkCss = import ../../theme/lib/mkCss.nix;
+  withTheme = x: ":root ${mkCss theme.colors}\n" + x;
 
   lines = with builtins; file: filter isString (split "\n" (readFile file));
   readSet = with builtins; file:
@@ -16,41 +17,41 @@ let
     in
       listToAttrs (map mkSet settings);
 
-  patchedVariables = ''
-    :root ${mkCss theme.colors}
-    ${builtins.readFile ./variables.css}
-    '';
+  withImports = with builtins; let
+    patchImports = file:
+      let
+        importLine = "@import";
+        isImport = x: substring 0 (stringLength importLine) x == importLine;
+        imports = x: concatLists (map (match (importLine + " \"(.*)\";")) (filter isImport (lines x)));
+        importing = x: withImports (dirOf file + ("/" + x));
+      in concatStringsSep "\n" (map importing (imports file));
 
-  patchCss = with builtins; let
-    patchImports = let
-      importLine = "@import";
-      isImport = x: substring 0 (stringLength importLine) x == importLine;
-      imports = x: concatLists (map (match (importLine + " \"(.*)\";")) (filter isImport (lines x)));
-      patching = file: x: if x == "global/variables.css" then patchedVariables else patchCss (dirOf file + ("/" + x));
-    in file: concatStringsSep "\n" (map (patching file) (imports file));
-
-    patchUrls = with builtins; file:
+    withUrls = file:
       let
         fileStr = readFile file;
         urls = concatLists (filter isList (split "url\\(([^\\.\\)]+\\.svg)\\)" fileStr));
         replacement = map (x: toString (dirOf file + ("/" + x))) urls;
       in replaceStrings urls replacement fileStr;
 
-  in file: patchUrls file + "\n" + patchImports file;
+  in file: withUrls file + "\n" + patchImports file;
 
-  patchedUserChrome = patchCss (
-    pkgs.fetchFromGitHub {
-      owner = "muckSponge";
-      repo = "materialFox";
-      rev = "f3d6ed009ef8e06bc91c799dc69471d0742f17f6";
-      sha256 = "0cc50c3q7nwcq3kag2rafchqndnnhpl6y4v7m62aiidnl4h5jhjw";
-    } + "/chrome/userChrome.css");
+  patchedUserChrome = withTheme
+    ''
+    ${withImports (
+      pkgs.fetchFromGitHub {
+        owner = "muckSponge";
+        repo = "materialFox";
+        rev = "f3d6ed009ef8e06bc91c799dc69471d0742f17f6";
+        sha256 = "0cc50c3q7nwcq3kag2rafchqndnnhpl6y4v7m62aiidnl4h5jhjw";
+      } + "/chrome/userChrome.css")}
+    ${builtins.readFile ./diffs.css}
+    '';
 in
 {
   programs.firefox = {
     enable = true;
     profiles = {
-      
+
       default = {
         id = 0;
         isDefault = true;
