e67e30a1ffb0406e8b496fd1b16e5d2f96b6d812
nothingelsematters <daretoodefy@gmail.com>
2020-02-02 23:24:10 +0300

    :fire: firefox: imports adequate solution

--- nixpkgs/programs/firefox/default.nix
+++ nixpkgs/programs/firefox/default.nix
@@ -9,7 +9,7 @@ let
   readSet = with builtins; file:
     let
       matcher = match "[[:space:]]*\"(.*)\"[[:space:]]*=[[:space:]]*(.*);.*";
-      settings = filter (x: isNull x != true && length x == 2) (map matcher (lines file));
+      settings = filter (x: !(isNull x) && length x == 2) (map matcher (lines file));
       mkSet = list: {
         name = elemAt list 0;
         value = fromJSON (elemAt list 1);
@@ -17,33 +17,17 @@ let
     in
       listToAttrs (map mkSet settings);
 
-  withImports = with builtins; let
-    patchImports = file:
-      let
-        importLine = "@import";
-        isImport = x: substring 0 (stringLength importLine) x == importLine;
-        imports = x: concatLists (map (match (importLine + " \"(.*)\";")) (filter isImport (lines x)));
-        importing = x: withImports (dirOf file + ("/" + x));
-      in concatStringsSep "\n" (map importing (imports file));
+  materialFox = pkgs.fetchFromGitHub {
+    owner = "muckSponge";
+    repo = "materialFox";
+    rev = "f3d6ed009ef8e06bc91c799dc69471d0742f17f6";
+    sha256 = "0cc50c3q7nwcq3kag2rafchqndnnhpl6y4v7m62aiidnl4h5jhjw";
+  } + "/chrome/userChrome.css";
 
-    withUrls = file:
-      let
-        fileStr = readFile file;
-        urls = concatLists (filter isList (split "url\\(([^\\.\\)]+\\.svg)\\)" fileStr));
-        replacement = map (x: toString (dirOf file + ("/" + x))) urls;
-      in replaceStrings urls replacement fileStr;
-
-  in file: withUrls file + "\n" + patchImports file;
-
-  patchedUserChrome = withTheme
+  patchedUserChrome =
     ''
-    ${withImports (
-      pkgs.fetchFromGitHub {
-        owner = "muckSponge";
-        repo = "materialFox";
-        rev = "f3d6ed009ef8e06bc91c799dc69471d0742f17f6";
-        sha256 = "0cc50c3q7nwcq3kag2rafchqndnnhpl6y4v7m62aiidnl4h5jhjw";
-      } + "/chrome/userChrome.css")}
+    @import "${materialFox}";
+    :root ${mkCss theme.colors}
     ${builtins.readFile ./overrides.css}
     '';
 in
