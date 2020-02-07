9ef88f6de3d52b8dfc71c54a9c8b336ee7dcbf14
nothingelsematters <daretoodefy@gmail.com>
2020-02-07 14:20:28 +0300

    :art: compton: little tweaks

--- nixpkgs/services/compton/default.nix
+++ nixpkgs/services/compton/default.nix
@@ -4,10 +4,16 @@
   services.compton = {
     enable = true;
 
+    inactiveDim = "0.1";
     inactiveOpacity = "0.87";
-    menuOpacity = "0.92";
+    menuOpacity = "0.8";
 
     blur = true;
+
+    shadow = true;
+    shadowExclude = [ "window_type *= 'menu'" "name ~= 'Telegram'" ];
+    shadowOpacity = "0.9";
+    shadowOffsets = [ (-20) (-20) ];
 
     fade = true;
     fadeDelta = 5;
