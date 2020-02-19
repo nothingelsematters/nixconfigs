0fb52720d72b5914cf265c3c2d5da6bef3aa13d8
nothingelsematters <daretoodefy@gmail.com>
2020-02-19 14:18:44 +0300

    :penguin: users: mutability changed

--- nixos/users.nix
+++ nixos/users.nix
@@ -3,9 +3,13 @@
 {
   users = {
     defaultUserShell = pkgs.zsh;
+    mutableUsers = false;
+
     users.simon = {
       createHome = true;
       isNormalUser = true;
+      hashedPassword =
+        "$6$90sWr84wO2$dD5YjotjN9o5s0ou5E82R.4OQIrTMwUQt7XcsWqShH0OOZER4CHyA4QJUJTyFSkTXjzpKklDsRfpvimvdxqz1/";
       extraGroups =
         [ "wheel" "networkmanager" "audio" "video" "docker" "input" ];
       description = "Simyon Empire";
