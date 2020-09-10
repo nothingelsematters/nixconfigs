3a5c4920e9d34d60f8d600284bb4c25a9bcf7019
Simon Naumov <daretoodefy@gmail.com>
2020-09-10 12:04:06 +0300

    :wrench: vscode: configuration updating

--- home/development/vscode/default.nix
+++ home/development/vscode/default.nix
@@ -6,15 +6,13 @@ args@{ config, pkgs, ... }:
     package = config.programs.vscode.package;
   };
 
-  xdg.configFile."Code/User/keybindings.json".text =
-    builtins.toJSON (import ./keybindings.nix);
-
   programs.vscode = {
     enable = true;
     haskell = {
       enable = true;
       hie.enable = false;
     };
+    keybindings = import ./keybindings.nix;
     extensions = import ./extensions.nix args;
     userSettings = import ./settings.nix args;
   };
