db6eac8b538030287b753922fe8780f946ac8806
Simon Naumov <daretoodefy@gmail.com>
2020-09-10 09:56:16 +0300

    :penguin: hardware: configuration changes

--- hosts/simyon/default.nix
+++ hosts/simyon/default.nix
@@ -3,25 +3,6 @@
 {
   imports = [ ./hardware.nix ../../home/simon.nix ../../services ];
 
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-    plymouth.enable = true;
-  };
-
-  hardware = {
-    enableAllFirmware = true;
-    pulseaudio = {
-      enable = true;
-      package = pkgs.pulseaudioFull;
-      support32Bit = true;
-    };
-
-    opengl.driSupport32Bit = true;
-  };
-
   system = {
     stateVersion = "20.03";
     autoUpgrade.enable = true;
@@ -29,17 +10,6 @@
 
   sound.enable = true;
   networking.networkmanager.enable = true;
-
-  nix = {
-    binaryCaches = [ "https://nothingelsematters.cachix.org" ];
-    binaryCachePublicKeys = [
-      "nothingelsematters.cachix.org-1:zRZaHQgixucyZdxxClIKICy1QHtTFGeGng//uxspSPQ="
-    ];
-
-    autoOptimiseStore = true;
-    gc.automatic = true;
-    optimise.automatic = true;
-  };
 
   time.timeZone = "Europe/Moscow";
 
@@ -69,15 +39,19 @@
     ];
   };
 
-  services = {
-    printing = {
-      enable = true;
-      drivers = [ pkgs.brlaser ];
-    };
+  nix = {
+    binaryCaches = [ "https://nothingelsematters.cachix.org" ];
+    binaryCachePublicKeys = [
+      "nothingelsematters.cachix.org-1:zRZaHQgixucyZdxxClIKICy1QHtTFGeGng//uxspSPQ="
+    ];
 
-    udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
-      ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
-    '';
+    autoOptimiseStore = true;
+    gc.automatic = true;
+    optimise.automatic = true;
   };
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
+  '';
 }
