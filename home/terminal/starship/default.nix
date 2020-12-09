7e96ad9b8feb8b055e893b4d71c3c7c797d1eac0
Simon Naumov <daretoodefy@gmail.com>
2020-12-09 21:28:35 +0300

    :art: kotlin starship custom module

--- home/terminal/starship/default.nix
+++ home/terminal/starship/default.nix
@@ -3,22 +3,31 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      add_newline = false;
       scan_timeout = 10;
+
+      memory_usage.disabled = false;
 
       cmd_duration = {
         min_time = 500;
         show_milliseconds = true;
       };
 
-      nix_shell = {
-        use_name = true;
-        symbol = "❄️";
-      };
+      nix_shell.use_name = true;
 
       python.python_binary = [ "python3" ];
 
-      memory_usage.disabled = false;
+      custom.kotlin = {
+        symbol = "🏝 ";
+        style = "bold fg:208";
+        format = "via [\${symbol}\${version}]($style)";
+        command = "(cat build.gradle.kts"
+          + " | rg 'id\\(\"org.jetbrains.kotlin.jvm\"\\) version'"
+          + " || kotlin -version)"
+          + " | gawk '{ gsub(/\"/, \"\", $3); print $3 }'";
+        files = [ "build.gradle.kts" ];
+        directories = [ "gradle" ];
+        extensions = [ "kt" ];
+      };
     };
   };
 }
