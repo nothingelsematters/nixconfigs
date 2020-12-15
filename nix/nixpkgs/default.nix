3932082fbe7053c68b43399cc970dced7fa42dcd
Simon Naumov <daretoodefy@gmail.com>
2020-12-15 19:33:12 +0300

    :bug: little improvement of packages structure

--- nix/nixpkgs/default.nix
+++ nix/nixpkgs/default.nix
@@ -4,8 +4,7 @@ let
   config = { allowBroken = false; };
   reliable-nixpkgs = import sources.reliable-nixpkgs { inherit config; };
   turbo-nixpkgs = import sources.turbo-nixpkgs { inherit config; };
-in with reliable-nixpkgs;
-with turbo-nixpkgs; {
+in {
   nixpkgs-wayland =
     import "${super.nixpkgs-wayland-sources}/default.nix" self super;
 }
