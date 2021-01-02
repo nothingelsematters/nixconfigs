80efd56b1f252abcfe7a14c4fca6dbcb2d191c55
Simon Naumov <daretoodefy@gmail.com>
2021-01-02 18:54:18 +0300

    :art: materia theme finally

--- home/theme/default.nix
+++ home/theme/default.nix
@@ -4,8 +4,6 @@ args@{ lib, pkgs, ... }:
   lib.theme = lib.trivial.pipe (import ../lib/imports.nix {
     inherit lib;
     dir = ./.;
-    includeDirectories = false;
     includeFiles = true;
-    additional = [ ./colors/github-dark.nix ];
   }) [ (builtins.map (x: import x args)) (builtins.foldl' (x: y: x // y) { }) ];
 }
