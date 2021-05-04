47319c2e5a203a0a77a8942da471f054fd358e8a
Simon Naumov <daretoodefy@gmail.com>
2021-05-04 23:13:48 +0300

    :sparkles: flutter: vscode extension

--- home/home/development/dart.nix
+++ home/home/development/dart.nix
@@ -1,16 +1,24 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.dart ];
+  home.packages = with pkgs; [ dart flutter ];
 
   programs.vscode = {
     userSettings."dart.checkForSdkUpdates" = false;
 
-    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-      name = "dart-code";
-      publisher = "dart-code";
-      version = "3.22.0";
-      sha256 = "sha256-1nTewVmlrxbXdRR1EPts46u24LHdnP5BblFsMaGlNYg=";
-    }];
+    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        publisher = "dart-code";
+        name = "flutter";
+        version = "3.22.0";
+        sha256 = "sha256-woygN6hOWlP2UayqwDhJh9KcZk1GzH7mDF5IueDRxs4=";
+      }
+      {
+        publisher = "dart-code";
+        name = "dart-code";
+        version = "3.22.0";
+        sha256 = "sha256-1nTewVmlrxbXdRR1EPts46u24LHdnP5BblFsMaGlNYg=";
+      }
+    ];
   };
 }
