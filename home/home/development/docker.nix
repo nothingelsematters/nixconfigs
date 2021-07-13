280ce9d1ff24b4ad03e2ee5a78fec3fe242585a5
Simon Naumov <daretoodefy@gmail.com>
2021-07-13 11:54:58 +0300

    :sparkles: new convenient docker compose alias

--- home/home/development/docker.nix
+++ home/home/development/docker.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  programs.zsh.shellAliases.dc = "docker-compose";
+  programs.zsh.shellAliases = {
+    dc = "docker-compose";
+    dc-restart = "dc down --remove-orphans --volumes && dc up --detach --build --force-recreate";
+  };
 
   home.packages = with pkgs; [ docker docker_compose ];
 }
