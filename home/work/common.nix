c7eadb695cfcd2f4e37fd7dae33ea4b75bdee799
Simon Naumov <daretoodefy@gmail.com>
2021-07-27 11:43:35 +0300

    :package: adding work kubernetes

--- home/work/common.nix
+++ home/work/common.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ ../common ];
+  imports = [ ../common ./starship.nix ];
 
-  home.packages = with pkgs; [ jdk11 maven ];
+  home.packages = with pkgs; [ jdk11 maven kubernetes ];
 
   lib.theme.isDark = true;
 
@@ -14,36 +14,5 @@
       name = "forgit";
       src = pkgs.inputs.forgit;
     }];
-
-    starship.settings = {
-      scan_timeout = 1;
-
-      memory_usage = {
-        disabled = false;
-        format = "ram ([\${ram}]($style)) ";
-      };
-
-      cmd_duration = {
-        min_time = 500;
-        show_milliseconds = true;
-      };
-
-      git_branch.symbol = "";
-      java.symbol = "java ";
-      jobs.symbol = "+";
-      nix_shell.symbol = "nix ";
-
-      env_var.variable = "NIX_NAME";
-
-      character = {
-        success_symbol = "[>](bold green)";
-        error_symbol = "[>](bold red)";
-        vicmd_symbol = "[<](bold green)";
-      };
-
-      format = "$shlvl" + "$directory" + "$git_branch" + "$git_commit"
-        + "$git_state" + "$docker_context" + "$nix_shell" + "$memory_usage"
-        + "$cmd_duration" + "$line_break" + "$jobs" + "$battery" + "$character";
-    };
   };
 }
