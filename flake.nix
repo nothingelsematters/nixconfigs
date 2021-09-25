a0153c017d30c8c39ef0ce98852fe8b1cdd899d2
Simon Naumov <daretoodefy@gmail.com>
2021-09-25 15:36:40 +0300

    :arrow_up: flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-turbo.url = "nixpkgs/master";
     nixpkgs-stable.url = "nixpkgs/release-21.05";
+    nixpkgs-fixed.url = "nixpkgs/ec81333c99894f6dd9b248efe617ff79482cc8f6";
     nixpkgs-wayland.url = "github:colemickens/nixpkgs-wayland";
     home.url = "github:nix-community/home-manager/master";
     flake-utils.url = "github:numtide/flake-utils";
@@ -22,7 +23,7 @@
   };
 
   outputs =
-    inputs@{ nixpkgs, nixpkgs-turbo, nixpkgs-stable, home, forgit, ... }:
+    inputs@{ nixpkgs, nixpkgs-turbo, nixpkgs-stable, nixpkgs-fixed, home, forgit, ... }:
     let username = "simon";
     in rec {
       # home, NixOS
@@ -36,10 +37,31 @@
         makeConfiguration = system: homeDirectory: file:
           home.lib.homeManagerConfiguration {
               inherit username system homeDirectory;
+
               configuration = { pkgs, lib, ... }: {
                 imports = [ file ];
+
                 nixpkgs = {
-                  overlays = [ (self: super: { inherit inputs; }) ];
+                  overlays = [
+                    (self: super: {
+                      inherit forgit;
+
+                      turbo = import nixpkgs-turbo {
+                        inherit system;
+                        config.allowUnfree = true;
+                      };
+
+                      stable = import nixpkgs-stable {
+                        inherit system;
+                        config.allowUnfree = true;
+                      };
+
+                      fixed = import nixpkgs-fixed {
+                        inherit system;
+                        config.allowUnfree = true;
+                      };
+                    })
+                  ];
                   config.allowUnfree = true;
                 };
               };
