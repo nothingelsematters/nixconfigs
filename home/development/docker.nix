fcb93f93173008e83d3491e0bb8c765a8e01c36e
Simon Naumov <daretoodefy@gmail.com>
2021-10-19 21:41:50 +0300

    :sparkles: docker launch alias

--- home/development/docker.nix
+++ home/development/docker.nix
@@ -5,6 +5,7 @@
     dc = "docker-compose";
     dc-restart =
       "dc down --remove-orphans --volumes && dc up --detach --build --force-recreate";
+    dlaunch = "open /Applications/Docker.app";
   };
 
   home.packages = with pkgs; [ docker docker_compose dive ];
