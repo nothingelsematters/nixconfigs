d28626047c5297ae6b3aaac87ec81e1fefe278bb
Simon Naumov <daretoodefy@gmail.com>
2021-11-09 00:08:56 +0300

    :arrow_up: python

--- home/development/python.nix
+++ home/development/python.nix
@@ -1,18 +1,32 @@
 { pkgs, config, ... }:
 
 {
-  home.packages = with pkgs; [
-    (python310.withPackages (ps: with ps; [ autopep8 ]))
+  home.packages = [
+    (pkgs.python39.withPackages (ps:
+      with ps; [
+        autopep8
+
+        notebook
+        ipykernel
+        pandas
+        scikitlearn
+        pyenchant
+        nltk
+      ]))
   ];
 
   programs = {
     zsh.shellAliases.py = "python3";
 
-    vscode.userSettings = {
-      "python.dataScience.alwaysTrustNotebooks" = true;
-      "python.dataScience.askForKernelRestart" = false;
-      "jupyter.experiments.optInfo" = [ "CustomEditor" ];
-      "jupyter.askForKernelRestart" = false;
+    vscode = {
+      extensions = with pkgs.vscode-extensions; [ ms-toolsai.jupyter ];
+
+      userSettings = {
+        "python.dataScience.alwaysTrustNotebooks" = true;
+        "python.dataScience.askForKernelRestart" = false;
+        "jupyter.experiments.optInfo" = [ "CustomEditor" ];
+        "jupyter.askForKernelRestart" = false;
+      };
     };
   };
 }
