42153cebbb4a982cea46da355f7561b71ea2cf7c
Simon Naumov <daretoodefy@gmail.com>
2021-12-16 21:10:54 +0300

    :heavy_plus_sign: kitty

--- home/theme.nix
+++ home/theme.nix
@@ -8,21 +8,40 @@ args@{ lib, pkgs, ... }:
 
     colors = rec {
       background = rec {
-        primary = "#1f2428";
-        secondary = "#2f363d";
-        disabled = "#2f363d";
-        accent = "#717790";
-        urgent = "#f44d4d";
+        primary = "#23282e";
+        secondary = "#1d476a";
+        disabled = "#2a3037";
+        urgent = "#ea4a5a";
         selection = secondary;
         inverted = text.primary;
       };
+
       text = rec {
-        primary = "#e2e2e4";
-        secondary = "#044289";
-        disabled = "#5b5d67";
+        primary = "#e0e3e8";
+        secondary = "#68737e";
+        disabled = "#68737e";
         urgent = background.urgent;
-        inverted = background.primary;
         selection = primary;
+        inverted = background.primary;
+      };
+
+      ansi = {
+        black = "#586069";
+        red = "#ea4a5a";
+        green = "#34d058";
+        yellow = "#ffea7f";
+        blue = "#2188ff";
+        magenta = "#b392f0";
+        cyan = "#39c5cf";
+        white = "#d1d5da";
+        blackBright = "#959da5";
+        redBright = "#f97583";
+        greenBright = "#85e89d";
+        yellowBright = "#ffea7f";
+        blueBright = "#79b8ff";
+        magentaBright = "#b392f0";
+        cyanBright = "#56d4dd";
+        whiteBright = "#fafbfc";
       };
     };
 
