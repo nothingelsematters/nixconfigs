6108a8432d6cf237325d36c6a7976441654b5624
Simon Naumov <daretoodefy@gmail.com>
2022-12-23 20:54:01 +0600

    :package: flutter

--- home/development/flutter.nix
+++ home/development/flutter.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
 {
-  # home.packages = [ pkgs.prev-stable.flutter ];
-  programs.vscode.extensions = [ pkgs.vscode-extensions.dart-code.flutter ];
+  home.packages = with pkgs;
+    [
+      # TODO https://github.com/NixOS/nixpkgs/pull/196133
+      # for now: https://docs.flutter.dev/get-started/install/macos
+      # flutter
+      cocoapods
+    ];
+
+  programs.vscode.extensions = with pkgs.vscode-extensions; [
+    dart-code.dart-code
+    dart-code.flutter
+  ];
 }
