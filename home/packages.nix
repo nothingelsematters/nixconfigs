93a611dbd7a03a543bb7336419c288df0cfb2bce
Simon Naumov <daretoodefy@gmail.com>
2023-01-10 14:12:48 +0600

    :heavy_minus_sign: swiftbar

--- home/packages.nix
+++ home/packages.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    # move and resize windows in macOS using keyboard shortcuts or snap areas
-    rectangle
-    # powerful macOS menu bar customization tool
-    swiftbar
-  ];
+  home.packages = with pkgs;
+    [
+      # move and resize windows in macOS using keyboard shortcuts or snap areas
+      rectangle
+    ];
 }
