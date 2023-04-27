c21b22140409a94029f3ddc9e59c17f65f0b7d66
Simon Naumov <daretoodefy@gmail.com>
2023-04-27 09:44:39 +0200

    :sparkles: ipython alias

--- home/development/python.nix
+++ home/development/python.nix
@@ -5,7 +5,10 @@
     [ (pkgs.python310.withPackages (ps: with ps; [ autopep8 jupyter scipy ])) ];
 
   programs = {
-    zsh.shellAliases.py = "python3";
+    zsh.shellAliases = {
+      py = "python3";
+      ipy = "ipython";
+    };
     vscode.extensions = [ pkgs.vscode-extensions.ms-toolsai.jupyter ];
   };
 }
