0790f0b724a68cc2fac7c72d2d731a448fc6661c
Simon Naumov <daretoodefy@gmail.com>
2023-07-08 11:58:03 +0600

    :arrow_up: flake, flutter

--- home/development/flutter.nix
+++ home/development/flutter.nix
@@ -3,7 +3,6 @@
 {
   home.packages = with pkgs;
     [
-      # TODO https://github.com/NixOS/nixpkgs/pull/196133
       # for now: https://docs.flutter.dev/get-started/install/macos
       # flutter
       cocoapods
