a30a2b17e6766709f769f753f01ac1c1d54505d7
Simon Naumov <daretoodefy@gmail.com>
2023-07-08 13:52:59 +0600

    :heavy_minus_sign: python jupyter

--- home/development/.python-jupyter.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  home.packages =
-    [ (pkgs.python311.withPackages (ps: with ps; [ autopep8 jupyter scipy ])) ];
-
-  programs.vscode.extensions = [ pkgs.vscode-extensions.ms-toolsai.jupyter ];
-}
