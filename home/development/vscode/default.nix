1fbf655d89b06cb4403aee6ce3f0c4dfd4798cfa
Simon Naumov <daretoodefy@gmail.com>
2023-09-18 16:59:57 +0600

    :bug: vscode

--- home/development/vscode/default.nix
+++ home/development/vscode/default.nix
@@ -7,6 +7,6 @@ args@{ config, pkgs, ... }:
     enable = true;
     extensions = import ./extensions.nix args;
     keybindings = import ./keybindings.nix;
-    userSettings = import ./settings.nix;
+    userSettings = import ./settings.nix args;
   };
 }
