67ff5a792ef93c4859e6d6e0a086a07fae8db6eb
Simon Naumov <daretoodefy@gmail.com>
2024-03-05 23:36:37 +0500

    :truck: git

---
+++ home/development/git/default.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+
+let editor = config.lib.packages.editor;
+in {
+  home.packages = with pkgs; [ nodePackages.gitmoji-cli lab pre-commit ];
+
+  programs = {
+    gh.enable = true;
+
+    zsh = rec {
+      plugins = [{
+        name = "forgit";
+        src = pkgs.zsh-forgit + /share/zsh/zsh-forgit;
+      }];
+
+      shellAliases = import ./aliases.nix editor;
+    };
+
+    git = {
+      enable = true;
+      userName = "Simon Naumov";
+
+      delta = {
+        enable = true;
+        options = {
+          line-numbers = true;
+          decorations = true;
+          theme = "OneHalfDark";
+        };
+      };
+
+      extraConfig = {
+        pull.rebase = true;
+        credential.helper = "store";
+        init.defaultBranch = "main";
+        advice.skippedCherryPicks = false;
+
+        core.editor = "${editor} --wait";
+        color.ui = true;
+
+        "color \"diff-highlight\"" = {
+          oldNormal = "red bold";
+          oldHighlight = "red bold 52";
+          newNormal = "green bold";
+          newHighlight = "green bold 52";
+        };
+
+        "color \"diff\"" = {
+          meta = "yellow";
+          frag = "magenta bold";
+          commit = "yellow bold";
+          old = "red bold";
+          new = "green bold";
+          whitespace = "red reverse";
+        };
+      };
+    };
+  };
+}
