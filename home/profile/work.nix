d0edb00e9c6eb4ee6f54e8bc904e9ab3ef6b1c2e
Simon Naumov <daretoodefy@gmail.com>
2024-05-27 15:12:25 +0500

    :wrench: openssl

--- home/profile/work.nix
+++ home/profile/work.nix
@@ -13,32 +13,34 @@
       "nix build .#work.activationPackage && ./result/activate";
   };
 
-  home = with pkgs; {
-    sessionVariables = {
-      OPENSSL_ROOT_DIR = "${openssl}";
-      PKG_CONFIG_PATH = "$PKG_CONFIG_PATH:${openssl.dev}/lib/pkgconfig";
-      # install oracle client manually because of tnsnames.ora
-      LD_LIBRARY_PATH =
-        "$LD_LIBRARY_PATH:/usr/lib/oracle/21/client64/lib:${libaio}/lib";
+  home = with pkgs;
+    let openssl_pkg = openssl_3_1;
+    in {
+      sessionVariables = {
+        OPENSSL_ROOT_DIR = "${openssl_pkg}";
+        PKG_CONFIG_PATH = "$PKG_CONFIG_PATH:${openssl_pkg.dev}/lib/pkgconfig";
+        # install oracle client manually because of tnsnames.ora
+        LD_LIBRARY_PATH =
+          "$LD_LIBRARY_PATH:/usr/lib/oracle/21/client64/lib:${libaio}/lib";
+      };
+
+      packages = [
+        # building
+        gcc
+        cmake
+        binutils
+        openssl_pkg.dev
+        openssl_pkg
+        turbo.zlib
+        protobuf
+        gawk
+        cyrus_sasl
+
+        # devopsing
+        etcd_3_4
+        stable.dbeaver
+        libaio # for oracle client
+        postgresql
+      ];
     };
-
-    packages = [
-      # building
-      gcc
-      cmake
-      binutils
-      openssl.dev
-      openssl
-      turbo.zlib
-      protobuf
-      gawk
-      cyrus_sasl
-
-      # devopsing
-      etcd_3_4
-      stable.dbeaver
-      libaio # for oracle client
-      postgresql
-    ];
-  };
 }
