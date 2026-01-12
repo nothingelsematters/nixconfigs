8c30a694b6841e2dd818b8e40870a822f4219642
Simon Naumov <simon.naumov.mail@gmail.com>
2026-01-12 13:52:47 +0100

    :wrench: starship

--- home/terminal/starship.nix
+++ home/terminal/starship.nix
@@ -1,59 +1,69 @@
 { lib, ... }:
 
 {
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
+  programs.starship =
+    let
+      git_style = "bold purple";
+    in
+    {
+      enable = true;
+      enableZshIntegration = true;
 
-    settings = {
-      add_newline = false;
-      scan_timeout = 10;
+      settings = {
+        add_newline = false;
+        scan_timeout = 10;
 
-      kubernetes = {
-        disabled = false;
-        format = ''[$symbol $context (\($namespace\))]($style)'';
-        symbol = "☁️";
+        git_branch.style = git_style;
+        git_state.style = git_style;
+
+        git_metrics.disabled = false;
+
+        git_status = {
+          style = git_style;
+          ahead = "⇡\${count}";
+          diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+          behind = "⇣\${count}";
+        };
+
+        memory_usage = {
+          format = "$symbol [\${ram}( | \${swap})]($style) ";
+          disabled = false;
+        };
+
+        cmd_duration = {
+          min_time = 500;
+          show_milliseconds = true;
+          format = ''⏱️ [$duration]($style)'';
+        };
+
+        nix_shell = {
+          format = "[$symbol]($style)";
+          symbol = "❄️";
+        };
+
+        shlvl = {
+          disabled = false;
+          threshold = 2;
+          symbol = "↓";
+          repeat = false;
+          style = "bold white";
+        };
+
+        format = lib.concatStrings [
+          "$directory"
+          "$git_branch"
+          "$git_state"
+          "$git_status"
+          "$git_metrics"
+          "$memory_usage"
+          "$cmd_duration"
+          "$line_break"
+          "$battery"
+          "$jobs"
+          "$nix_shell"
+          "$shlvl"
+          "$character"
+        ];
       };
-
-      memory_usage = {
-        format = "$symbol [\${ram}( | \${swap})]($style) ";
-        disabled = false;
-      };
-
-      cmd_duration = {
-        min_time = 500;
-        show_milliseconds = true;
-        format = ''\(took [$duration]($style)\)'';
-      };
-
-      nix_shell = {
-        format = "[$symbol]($style)";
-        symbol = "❄️";
-      };
-
-      shlvl = {
-        disabled = false;
-        threshold = 2;
-        symbol = "↓";
-        repeat = false;
-        style = "bold white";
-      };
-
-      format = lib.concatStrings [
-        "$directory"
-        "$git_branch"
-        "$git_state"
-        "$git_status"
-        "$kubernetes"
-        "$memory_usage"
-        "$cmd_duration"
-        "$line_break"
-        "$battery"
-        "$jobs"
-        "$nix_shell"
-        "$shlvl"
-        "$character"
-      ];
     };
-  };
 }
